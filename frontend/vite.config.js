f4cf24f39ac9d480ab6f2359037fc9f550295668
Thiare <thiare.cerpa@gmail.com>
2025-03-19 23:19:49 -0300

    Guardando los cambios antes del pull

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
+  server: {
+    port: 5173,
+  },
+
   // server: {
   //   proxy: {
   //     "/api": {
